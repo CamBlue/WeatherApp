779d7814824fab341e6073a238f5eb69682a4770
Cameron <bamclue@gmail.com>
2024-05-27 16:18:41 -0600

    Added a couple lines, working out functionality

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from flask import Flask
-import requests
+import flask;
+import requests;
+import os;
 
-app = Flask(__name__)
+API_KEY = "czqy38iyh3P8Jhoni0E0XSisPetjTcEP"
 
+LocationName = input("Where are you located?\n")
+
+
+
+
+WeatherDataCall = requests.get('https://api.tomorrow.io/v4/weather/location=${LocationName}&apikey=czqy38iyh3P8Jhoni0E0XSisPetjTcEP')
+
+WeatherDataBlob = WeatherDataCall.json()
+
+WeatherTemperature = WeatherDataBlob['timelines']['minutely'][0]['values']['temperatureApparent']
+
+
+print(WeatherDataBlob)
