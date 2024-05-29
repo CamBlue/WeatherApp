6b2a78d11bfd76de693a19e6c81e92568ded9050
Cameron <bamclue@gmail.com>
2024-05-28 21:46:03 -0600

    trying some geolocater stuff

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import flask;
 import requests;
-import os;
+import os
+from geopy.geocoders import Nominatim
 
-API_KEY = "czqy38iyh3P8Jhoni0E0XSisPetjTcEP"
+#API_KEY = "czqy38iyh3P8Jhoni0E0XSisPetjTcEP"
+
+loc = Nominatim(user_agent="GetLoc")
+
 
 LocationName = input("Where are you located?\n")
 
-
-
-
+getLoc = loc.geocode("LocationName")
+print(getLoc)
+'''
 WeatherDataCall = requests.get('https://api.tomorrow.io/v4/weather/location=${LocationName}&apikey=czqy38iyh3P8Jhoni0E0XSisPetjTcEP')
 
 WeatherDataBlob = WeatherDataCall.json()
@@ -17,3 +21,4 @@ WeatherTemperature = WeatherDataBlob['timelines']['minutely'][0]['values']['temp
 
 
 print(WeatherDataBlob)
+'''
